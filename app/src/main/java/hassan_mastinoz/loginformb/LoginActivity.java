53eff5bbe3cf3e40fc7e2fd82dee251552237cf7
mastinoz <persiste1@gmail.com>
2016-09-07 19:19:07 +0100

    27 Designing Arabic Layout 1

--- app/src/main/java/hassan_mastinoz/loginformb/LoginActivity.java
+++ app/src/main/java/hassan_mastinoz/loginformb/LoginActivity.java
@@ -17,34 +17,6 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        EditText username = (EditText) findViewById(R.id.usernameET);
-
-        EditText password = (EditText) findViewById(R.id.passwordET);
-
-        String getCurrentLocale = Locale.getDefault().getDisplayLanguage();
-
-        if(getCurrentLocale.equalsIgnoreCase("English")){          //LTR languages
-
-            username.setGravity(Gravity.LEFT);
-
-            username.setCompoundDrawablesWithIntrinsicBounds(R.drawable.user_ic, 0, 0, 0);
-
-            password.setGravity(Gravity.LEFT);
-
-            password.setCompoundDrawablesWithIntrinsicBounds(R.drawable.password_ic, 0, 0, 0);
-
-        }else{                                                     //RTL languages
-
-            username.setGravity(Gravity.RIGHT);
-
-            username.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.user_ic, 0);
-
-            password.setGravity(Gravity.RIGHT);
-
-            password.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.password_ic, 0);
-
-        }
-
     }
 
 }
