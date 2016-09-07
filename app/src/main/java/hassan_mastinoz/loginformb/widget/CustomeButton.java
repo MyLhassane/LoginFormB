9e6c3028b56445c1cc0eaaf4688d76740865ff42
mastinoz <persiste1@gmail.com>
2016-09-07 18:09:29 +0100

    26 - adding fonts to custom button

---
+++ app/src/main/java/hassan_mastinoz/loginformb/widget/CustomeButton.java
@@ -0,0 +1,64 @@
+package hassan_mastinoz.loginformb.widget;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+import hassan_mastinoz.loginformb.R;
+
+/**
+ * Created by hassan on 9/7/16.
+ */
+public class CustomeButton extends TextView {
+
+    public CustomeButton(Context context, AttributeSet attrs, int defStyle)
+    {
+        super(context, attrs, defStyle);
+        init(attrs);
+    }
+
+    public CustomeButton(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+        init(attrs);
+
+    }
+
+    public CustomeButton(Context context)
+    {
+        super(context);
+        init(null);
+
+    }
+
+    private void init(AttributeSet attrs)
+    {
+        if (attrs!=null)
+        {
+            TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.CustomeFont);
+            String fontName = a.getString(R.styleable.CustomeFont_AraJozoor_Regular);
+            if (Locale.getDefault().getDisplayLanguage().toString().equals("Arabic"))
+            {
+
+
+            }
+            else
+            {
+                a = getContext().obtainStyledAttributes(attrs, R.styleable.CustomeFont);
+                fontName = a.getString(R.styleable.CustomeFont_armalite_rifle);
+            }
+            if (fontName!=null)
+            {
+                Typeface myTypeface = Typeface.createFromAsset(getContext().getAssets(), "fonts/" +fontName);
+                setTypeface(myTypeface);
+            }
+            a.recycle();
+        }
+
+    }
+
+}
